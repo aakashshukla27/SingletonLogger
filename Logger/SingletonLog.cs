78ccf4fb96eacb191a9764a13fa630ae3c428441
Aakash Shukla <shukla.aa@northeastern.edu>
2021-09-28 01:31:56 -0400

    Add project files.

---
+++ Logger/SingletonLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public sealed class SingletonLog : ILog
+    {
+        private SingletonLog() { }
+
+        private static readonly Lazy<SingletonLog> instance = new Lazy<SingletonLog>(() => new SingletonLog());
+
+        public static SingletonLog GetInstance()
+        {
+            return instance.Value;
+        }
+
+        public void LogException(string message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(message);
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
---
+++ LoggerLibrary/SingletonLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerLibrary
+{
+    public sealed class SingletonLogger : ILog
+    {
+        private SingletonLogger() { }
+
+        private static readonly Lazy<SingletonLogger> instance = new Lazy<SingletonLogger>(() => new SingletonLogger());
+
+        public static SingletonLogger GetInstance()
+        {
+            return instance.Value;
+        }
+
+        public void LogException(string message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(message);
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
